27e2523ab3dca415544d1de3c257851d8e627c78
NasirMarwat <nasirkhani030859@gmail.com>
2024-03-11 10:26:43 +0500

    first commit

---
+++ src/todo/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h2>To-Do List</h2>
+      <div>
+        <input
+          type='text'
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder='Enter Task...'
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
